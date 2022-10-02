b41f9290021c2378242d10817c43541205632469
Алексей Трифонов <atrifonov2@rencredit.ru>
2022-10-02 15:39:38 +0300

    Added ssh key management example

--- main.py
+++ main.py
@@ -22,3 +22,19 @@ if __name__ == '__main__':
     for server in servers:
         print(server)
 
+    while True:
+        action = input('What do you want to do? ((S)how|(A)dd|(D)elete|(E)xit ')
+        if action in ('Show', 'S'):
+            for ssh_key in account.ssh_keys:
+                print(ssh_key)
+        elif action in ('Add', 'A'):
+            ssh_key_name = input('Enter ssh key name: ')
+            ssh_key_fingerprint = input('Enter public ssh key: ')
+            account.add_ssh_key(ssh_key_name, ssh_key_fingerprint)
+        elif action in ('Delete', 'D'):
+            ssh_key_id = input('Which key you want to delete? (id) ')
+            account.delete_ssh_key(int(ssh_key_id))
+        else:
+            break
+
+
