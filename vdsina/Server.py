f66dcfa1252250fa568bae34d35d91c5a7552f2b
Алексей Трифонов <atrifonov2@rencredit.ru>
2022-10-02 18:30:31 +0300

    Enriched the server class with new parameters. Added a docstring

--- vdsina/Server.py
+++ vdsina/Server.py
@@ -2,18 +2,44 @@ import ipaddress
 
 
 class Server:
+    """
+    Class to represent server information
+    Args:
+        server (dict): server representation from Account class
+    """
+    ip_addresses = []
+
     def __init__(self, server):
         self.rights = server['can']
         self.datacenter = server['datacenter']
-        self.fullname = server['full_name']
         self.id = server['id']
         self.name = server['name']
         self.plan = server['server-plan']
         self.status = server['status']
         self.template = server['template']
-        self.ipaddress = str(ipaddress.IPv4Network(f'{server["ip"]["ip"]}/{server["ip"]["netmask"]}', False))
-        self.fqdn = server['ip']['host']
-        self.gateway = server['ip']['gateway']
+        self.fqdn = server['host']
+        self.get_ips(server['ip'])
+        self.resources = server['data']
+        self.server_plan = server['server-plan']
+        self.server_group = server['server-group']
+        self.template = server['template']
+        self.datacenter = server['datacenter']
+        self.ssh_key = server['ssh-key']
+        self.bandwidth = server['bandwidth']
 
     def __str__(self) -> str:
-        return f'name: {self.name}\nip: {self.ipaddress}\nfqdn: {self.fqdn}'
+        result = f'name: {self.name}\nfqdn: {self.fqdn}\nIP addresses:\n'
+        for ip in self.ip_addresses:
+            for key in ip.keys():
+                result = f'{result}  {key}: {ip[key]}\n'
+        result = f'{result}Resources:\n'
+        for key in self.resources:
+            resource = self.resources[key]
+            result = f'{result} {key}: {resource["value"]} {resource["for"]}\n'
+        return result
+
+    def get_ips(self, ips_raw):
+        for ip in ips_raw:
+            self.ip_addresses.append(
+                {'ip': ipaddress.IPv4Network(f'{ip["ip"]}/{ip["netmask"]}', False),
+                 'gateway': ip['gateway'], 'mac': ip['mac']})
